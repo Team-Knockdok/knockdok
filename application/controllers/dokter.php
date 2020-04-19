28d92bb3ff88c587ebf4737e2b1223e854fd8fb6
Jordi Yaputra <Jordiyaputra@gmail.com>
2020-04-19 13:52:41 +0700

    Fixed dokter controller login bug (minor)

--- application/controllers/dokter.php
+++ application/controllers/dokter.php
@@ -3,25 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dokter extends CI_Controller {
 
-
 	public function __construct()
 	{
 		parent::__construct();
 		$this->load->model('dokter_model');
 	}
 
-
 	public function index()
 	{
-		if ($this->session->userdata('logged_in') == TRUE) {
-			$this->session->set_flashdata('failed', 'Anda sudah Login! <b>'.$this->session->userdata('nama_user').'</b>');
-			redirect('home');
-		} else {
-			$data['main_view'] = 'dokter_view';
-			$data['title'] = 'Dokter';
-			$data['list_dokter'] = $this->dokter_model->get();
-			$this->load->view('template', $data);
-		}
+		$data['main_view'] = 'dokter_view';
+		$data['title'] = 'Dokter';
+		$data['list_dokter'] = $this->dokter_model->get();
+		$this->load->view('template', $data);
 	}
 
 }
