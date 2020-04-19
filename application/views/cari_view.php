e4e5996d6db46f784b71d0418ef34589b8566b43
Jordi Yaputra <Jordiyaputra@gmail.com>
2020-04-19 17:58:57 +0700

    Updated cari view

--- application/views/cari_view.php
+++ application/views/cari_view.php
@@ -3,7 +3,7 @@
 <!-- Search form -->
 <form class="form-inline d-flex justify-content-center md-form form-sm mt-0">
     <button type="submit" class="form-control btn btn-info">Telusuri</button>
-    <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Cari nama dokter atau rumah sakit. Contoh: RS Indah" aria-label="Search">
+    <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Cari nama dokter atau rumah sakit. Contoh: rs Indah" aria-label="Search">
 </form>
 <br><br>
 
@@ -16,7 +16,7 @@
       <div class="card-body">
         <a href="<?= base_url()."dokter/profil/".$dokter["id_dokter"] ?>">
           <center>
-            <img class="card-img-top" src="https://image.freepik.com/free-vector/doctor-character-background_1270-84.jpg" height="180px" alt="Card image cap">
+            <img class="card-img-top" src="<?= base_url()."uploads/dokter/".$dokter["foto_dokter"] ?>" height="180px" alt="Foto Profil">
             <h5 class="card-title my-4" style="color:#579ca1;"><?= $dokter["nama_dokter"] ?></h5>
           </center>
         </a>
@@ -39,10 +39,10 @@
   <?php foreach ($query["list_rs"] as $rs): ?>
     <div class="card col-md-3">
       <div class="card-body">
-        <a href="<?= base_url()."rs/profil/".$rs["id_RS"] ?>">
+        <a href="<?= base_url()."rs/profil/".$rs["id_rs"] ?>">
           <center>
-            <img class="card-img-top" src="https://img.inews.id/media/822/files/inews_new/2019/08/06/06___Hospital.jpg" height="180px" alt="Card image cap">
-            <h5 class="card-title my-4" style="color:#579ca1;">RS <?= $rs["nama_RS"] ?></h5>
+            <img class="card-img-top" src="<?= base_url()."uploads/rs/".$rs["foto_rs"] ?>" height="180px" alt="Foto rs">
+            <h5 class="card-title my-4" style="color:#579ca1;">RS. <?= $rs["nama_rs"] ?></h5>
           </center>
         </a>
       </div>
