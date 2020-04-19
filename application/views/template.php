f6bdabbb93167de377a1cd93702b987e06a7884f
Ilham Izzul <ilhamizzul@gmail.com>
2020-04-19 11:36:24 +0700

    fixing navbar

--- application/views/template.php
+++ application/views/template.php
@@ -3,7 +3,6 @@
 
 <head>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/style.css">
 
     <title>Login</title>
@@ -21,22 +20,38 @@
                     <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Cari Dokter?</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="nav-link dropdown-toggle" href="#dokter" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Cari Dokter?</a>
+                    <div id="dokter" class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="#">Profil Dokter</a>
                         <a class="dropdown-item" href="#">Profil Rumah Sakit</a>
                     </div>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pembayaran</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Akun</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Profil</a>
-                        <a class="dropdown-item" href="#">Riwayat</a>
-                        <a class="dropdown-item" href="#">Bantuan</a>
-                </li>
+                <?php 
+                    if ($this->session->userdata('logged_in') == TRUE) {
+                        echo '
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pembayaran</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#akun" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Akun</a>
+                            <div class="dropdown-menu" id="akun" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Profil</a>
+                                <a class="dropdown-item" href="#">Riwayat</a>
+                                <a class="dropdown-item" href="#">Bantuan</a>
+                                <a class="dropdown-item" href="'.base_url().'auth/logout">Logout</a>
+                            </div>
+                        </li>
+                        ';
+                    } else {
+                        echo '
+                        <li class="nav-item">
+                            <a class="nav-link" href="'.base_url().'auth/">Login</a>
+                        </li>
+                        ';
+                    }
+                    
+                ?>
+                
             </ul>
         </div>
     </nav>
@@ -67,7 +82,8 @@
         ?>
         <?php $this->load->view($main_view); ?>
     </div>
-
+    
 </body>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </html>
