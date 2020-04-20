0fb137c316b6cb8d2988ec1f477cefb31e7a42bd
Jordi Yaputra <Jordiyaputra@gmail.com>
2020-04-20 12:42:23 +0700

    refactored rumah sakit to rs

--- application/controllers/cari.php
+++ application/controllers/cari.php
@@ -6,7 +6,7 @@ class Cari extends CI_Controller {
   public function __construct() {
     parent::__construct();
     $this->load->model('dokter_model');
-    $this->load->model('rumah_sakit_model');
+    $this->load->model('rs_model');
   }
 
   public function index() {
@@ -14,7 +14,7 @@ class Cari extends CI_Controller {
     $data['main_view'] = 'cari_view';
     $data['query'] = array(
       "list_dokter" => $this->dokter_model->get(),
-      "list_rs"     => $this->rumah_sakit_model->get()
+      "list_rs"     => $this->rs_model->get()
     );
     $this->load->view('template', $data);
   }
