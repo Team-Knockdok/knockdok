22777762adea96fd849a9e214a93351a504db3f9
Jordi Yaputra <Jordiyaputra@gmail.com>
2020-04-20 10:48:37 +0700

    added dokter model api get rs

--- application/models/dokter_model.php
+++ application/models/dokter_model.php
@@ -4,8 +4,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class dokter_model extends CI_Model {
 
   public function get() {
-    return $this->db->get('tb_dokter')
-                    ->result_array();
+    return $this->db->get('tb_dokter')->result_array();
+  }
+
+  public function getById($id) {
+    return $this->db
+      ->where('id_dokter', $id)
+      ->get('tb_dokter')
+      ->row_array();
+  }
+
+  public function getRsById($id) {
+    return $this->db
+      ->select('tb_rs.*')
+      ->where('id_dokter', $id)
+      ->from('tb_dokter')
+      ->join('tb_pivot_dokter_rs', 'id_dokter', 'left outer')
+      ->join('tb_rs', 'id_rs')
+      ->get()
+      ->result_array();
   }
 
 }
