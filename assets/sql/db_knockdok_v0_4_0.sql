bef0afc35339bb871f55b3cd0d6ca65c83fa1150
Jordi Yaputra <Jordiyaputra@gmail.com>
2020-04-25 19:28:39 +0700

    added new db sql

---
+++ assets/sql/db_knockdok_v0_4_0.sql
@@ -0,0 +1,244 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 25, 2020 at 02:21 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_knockdok`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_dokter`
+--
+
+CREATE TABLE `tb_dokter` (
+  `id_dokter` int(11) NOT NULL,
+  `nama_dokter` varchar(45) NOT NULL,
+  `spesialis` varchar(40) NOT NULL,
+  `nomor_telepon` varchar(12) NOT NULL,
+  `alamat_dokter` varchar(150) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `foto_dokter` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jadwal`
+--
+
+CREATE TABLE `tb_jadwal` (
+  `id_jadwal` int(11) NOT NULL,
+  `waktu_mulai` datetime NOT NULL,
+  `estimasi_durasi` time NOT NULL,
+  `tersedia` tinyint(1) NOT NULL DEFAULT 1,
+  `id_dokter` int(11) NOT NULL,
+  `id_rs` int(11) NOT NULL,
+  `biaya` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pesanan`
+--
+
+CREATE TABLE `tb_pesanan` (
+  `id_pesanan` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `id_jadwal` int(11) NOT NULL,
+  `keluhan` varchar(200) DEFAULT NULL,
+  `tanggal_pemesanan` date NOT NULL,
+  `id_transaksi` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pivot_dokter_rs`
+--
+
+CREATE TABLE `tb_pivot_dokter_rs` (
+  `id_dokter` int(11) NOT NULL,
+  `id_rs` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_rs`
+--
+
+CREATE TABLE `tb_rs` (
+  `id_rs` int(11) NOT NULL,
+  `nama_rs` varchar(30) NOT NULL,
+  `nomor_telepon_rs` varchar(12) NOT NULL,
+  `alamat_rs` varchar(100) NOT NULL,
+  `foto_rs` varchar(30) NOT NULL,
+  `delete_status` enum('true','false') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_transaksi`
+--
+
+CREATE TABLE `tb_transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `waktu_transaksi` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status_bayar` enum('pending','gagal','berhasil') DEFAULT 'pending',
+  `bukti_pembayaran` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `username` varchar(20) NOT NULL,
+  `nama_depan` varchar(15) NOT NULL,
+  `nama_belakang` varchar(15) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `email` varchar(35) NOT NULL,
+  `nomor_telepon` varchar(12) NOT NULL,
+  `foto_profil` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_dokter`
+--
+ALTER TABLE `tb_dokter`
+  ADD PRIMARY KEY (`id_dokter`);
+
+--
+-- Indexes for table `tb_jadwal`
+--
+ALTER TABLE `tb_jadwal`
+  ADD PRIMARY KEY (`id_jadwal`),
+  ADD KEY `id_dokter` (`id_dokter`),
+  ADD KEY `id_RS` (`id_rs`);
+
+--
+-- Indexes for table `tb_pesanan`
+--
+ALTER TABLE `tb_pesanan`
+  ADD PRIMARY KEY (`id_pesanan`),
+  ADD KEY `fk_username` (`username`),
+  ADD KEY `fk_id_jadwal` (`id_jadwal`),
+  ADD KEY `fk_id_transaksi` (`id_transaksi`);
+
+--
+-- Indexes for table `tb_pivot_dokter_rs`
+--
+ALTER TABLE `tb_pivot_dokter_rs`
+  ADD KEY `id_dokter` (`id_dokter`),
+  ADD KEY `id_RS` (`id_rs`);
+
+--
+-- Indexes for table `tb_rs`
+--
+ALTER TABLE `tb_rs`
+  ADD PRIMARY KEY (`id_rs`);
+
+--
+-- Indexes for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_dokter`
+--
+ALTER TABLE `tb_dokter`
+  MODIFY `id_dokter` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_jadwal`
+--
+ALTER TABLE `tb_jadwal`
+  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_pesanan`
+--
+ALTER TABLE `tb_pesanan`
+  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_rs`
+--
+ALTER TABLE `tb_rs`
+  MODIFY `id_rs` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_jadwal`
+--
+ALTER TABLE `tb_jadwal`
+  ADD CONSTRAINT `tb_jadwal_ibfk_1` FOREIGN KEY (`id_rs`) REFERENCES `tb_rs` (`id_rs`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tb_jadwal_ibfk_3` FOREIGN KEY (`id_rs`) REFERENCES `tb_rs` (`id_rs`) ON DELETE NO ACTION,
+  ADD CONSTRAINT `tb_jadwal_ibfk_4` FOREIGN KEY (`id_dokter`) REFERENCES `tb_dokter` (`id_dokter`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `tb_pesanan`
+--
+ALTER TABLE `tb_pesanan`
+  ADD CONSTRAINT `fk_id_jadwal` FOREIGN KEY (`id_jadwal`) REFERENCES `tb_jadwal` (`id_jadwal`),
+  ADD CONSTRAINT `fk_id_transaksi` FOREIGN KEY (`id_transaksi`) REFERENCES `tb_transaksi` (`id_transaksi`),
+  ADD CONSTRAINT `fk_username` FOREIGN KEY (`username`) REFERENCES `tb_user` (`username`);
+
+--
+-- Constraints for table `tb_pivot_dokter_rs`
+--
+ALTER TABLE `tb_pivot_dokter_rs`
+  ADD CONSTRAINT `tb_pivot_dokter_rs_ibfk_1` FOREIGN KEY (`id_dokter`) REFERENCES `tb_dokter` (`id_dokter`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_pivot_dokter_rs_ibfk_2` FOREIGN KEY (`id_rs`) REFERENCES `tb_rs` (`id_rs`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
