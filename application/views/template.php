d3001b3e758b2364b23b27322a5a41772b372459
Jordi Yaputra <Jordiyaputra@gmail.com>
2020-04-25 19:19:11 +0700

    removed mbs styles, ajax from template

--- application/views/template.php
+++ application/views/template.php
@@ -2,13 +2,13 @@
 <html>
 
 <head>
+    <title>KnockDok</title>
+    <!-- <link rel="stylesheet" type="text/css" href="<?= base_url('assets/vendor/materialDesign/css/mdb.min.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/vendor/materialDesign/css/style.css') ?>"> -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/style.css">
     <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/vendor/DataTables/datatables.css">
     <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/vendor/DataTables/DataTables/css/jquery.dataTables.min.css">
-    <title>KnockDok</title>
-    <link rel="stylesheet" href="<?= base_url('assets/vendor/materialDesign/css/mdb.min.css') ?>">
-    <link rel="stylesheet" href="<?= base_url('assets/vendor/materialDesign/css/style.css') ?>">
     <!-- JQuery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
 </head>
@@ -92,47 +92,11 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script type="text/javascript" charset="utf8" src="<?= base_url() ?>assets/vendor/DataTables/datatables.js"></script>
     <script type="text/javascript" charset="utf8" src="<?= base_url() ?>assets/vendor/DataTables/DataTables/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="<?= base_url('assets/vendor/materialDesign//js/mdb.min.js') ?>"></script>
+    <script type="text/javascript" src="<?= base_url('assets/vendor/materialDesign/js/mdb.min.js') ?>"></script>
     <script>
-        $(document).ready( function () {
+        $(document).ready(() => {
             $('#dataTable').DataTable();
-            <?php
-                if ($this->uri->segment(2) == "schedule") {
-                    echo '
-                    $("#dataTableJadwal").DataTable({
-                        "searching" : false,
-                        "ajax" : {
-                            "url" : "'.base_url().'dokter/get_schedule/'.$data_dokter["id_dokter"].'",
-                            "type" : "GET",
-                            "dataSrc" : ""
-                        },
-                        "columns" : [
-                            { "data" : "nama_rs" },
-                            { "data" : "waktu_mulai" },
-                            { "data" : "estimasi_durasi" },
-                            {
-                                "data" : "id_jadwal",
-                                render: function (dataField) {
-                                    return '.'\'<button type="button" class="btn btn-info" data-toggle="modal" data-target="#pesanmodal" onclick="pesan_jadwal(\'+dataField+\')">Pesan</button>\';
-                                }
-                            }
-                            ]
-                        });
-                        ';
-                    }
-                    ?>
-
-        } );
-
-        function pesan_jadwal(id) {
-            $.getJSON('<?= base_url() ?>dokter/get_schedule_by_id/'+id, function (data) {
-                $('#nama_dokter').text(data.nama_dokter)
-                $('#nama_rs').text(data.nama_rs)
-                $('#jadwal').text(data.waktu_mulai)
-                $('#durasi').text(data.estimasi_durasi)
-                $('#form_pesan_jadwal').attr('action', '<?= base_url() ?>dokter/pesan_schedule/'+id);
-            })
-        }
+        });
     </script>
 </body>
 </html>
